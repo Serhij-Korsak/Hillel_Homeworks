fb65b58db62c2d6994122aaf1222be717e324e97
Serhii Korsak <tiverec1922@gmail.com>
2022-02-19 23:54:53 +0200

    init commit

---
+++ src/com/hillel/lessons/lesson1/LessonOne.java
@@ -0,0 +1,10 @@
+package com.hillel.lessons.lesson1;
+
+public class LessonOne {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.println("this is my first try in Java!");
+        System.out.println("Made by Serhij Korsak");
+        System.out.println("26 Jan 2022");
+    }
+}
